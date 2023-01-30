a2f3bc606ac66f2e6f4e365d89022c20d94fd7ca
piekaa <piekoszek@gmail.com>
2023-01-30 20:43:48 +0100

    LCD screen

---
+++ Assets/Scripts/badge/Badge.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Badge : EventListener
+{
+    [SerializeField] private Text text;
+
+    [SerializeField] private Material badgeNameMaterial;
+    
+    
+    // https://forum.unity.com/threads/ui-text-to-texture.397135/
+    
+    private void LateUpdate()
+    {
+        badgeNameMaterial.mainTexture = text.mainTexture;
+    }
+}
